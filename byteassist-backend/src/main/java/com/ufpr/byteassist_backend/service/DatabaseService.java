d155839229a100c060bc35ed81eba9183aa3ad4b
VictorPBilbao <victor.pasini.bilbao@gmail.com>
2025-03-26 20:41:45 -0300

    update: configure database connection using dotenv and update CI workflow

--- byteassist-backend/src/main/java/com/ufpr/byteassist_backend/service/DatabaseService.java
+++ byteassist-backend/src/main/java/com/ufpr/byteassist_backend/service/DatabaseService.java
@@ -2,6 +2,7 @@ package com.ufpr.byteassist_backend.service;
 
 import com.surrealdb.Surreal;
 import com.surrealdb.signin.Database;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -15,10 +16,17 @@ public class DatabaseService {
     @PostConstruct
     public void initialize() {
         try {
+            Dotenv dotenv = Dotenv.load();
+            String url = dotenv.get("DATABASE_URL");
+            String namespace = dotenv.get("DATABASE_NAMESPACE");
+            String dbName = dotenv.get("DATABASE_NAME");
+            String user = dotenv.get("DATABASE_USER");
+            String password = dotenv.get("DATABASE_PASSWORD");
+
             db = new Surreal();
-            db.connect("wss://byteassist-database.fly.dev/");
-            db.useNs("dev").useDb("public");
-            db.signin(new Database("byteassist-api", "9g35IwQ*", "dev", "public"));
+            db.connect(url);
+            db.useNs(namespace).useDb(dbName);
+            db.signin(new Database(user, password, namespace, dbName));
 
         } catch (Exception e) {
             throw new RuntimeException("Failed to connect to the database", e);
