cbbe8787844159e8ddd79ba5a0536586f39f7d84
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-03-28 14:47:12 -0300

    Adicionar componente de cabeçalho e página de login; remover serviços não utilizados

--- byteassist-frontend/src/app/app.component.ts
+++ byteassist-frontend/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './features/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'byteassist-frontend';
+
+  constructor(private router: Router) {}
+  shouldShowHeader(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl === '/' ||
+           (!currentUrl.includes('/login') &&
+            !currentUrl.includes('/register') &&
+            !currentUrl.includes('/reset-password'));
+  }
 }
