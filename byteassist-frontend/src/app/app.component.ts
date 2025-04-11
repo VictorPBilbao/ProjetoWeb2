ea6694e0f476fd4e487a445abe97975e8931b606
ths <thalitasanttos77@gmail.com>
2025-04-11 16:11:58 -0300

    feat(app): update main component structure
    Updated the app.component.html and app.component.ts to improve layout and prepare for new routing structure.

--- byteassist-frontend/src/app/app.component.ts
+++ byteassist-frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { Router, RouterOutlet, NavigationEnd} from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './features/components/header/header.component';
+import { MenuSidebarComponent } from './features/components/menu-sidebar/menu-sidebar.component';
+import { AuthService } from './features/services/auth/auth.service';
 import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { filter } from 'rxjs/operators';
   imports: [
     RouterOutlet,
     HeaderComponent,
+    MenuSidebarComponent,
     CommonModule
   ],
   templateUrl: './app.component.html',
@@ -19,25 +21,11 @@ import { filter } from 'rxjs/operators';
 export class AppComponent {
   title = 'byteassist-frontend';
   activeSection: string = 'home';
-  showHeader: boolean = true;
 
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        //const noHeaderRoutes = ['/dashboard', '/register']; // Rotas sem header
-        const noHeaderRoutes = ['/dashboard','/new-request'];
-        this.showHeader = !noHeaderRoutes.some(route => 
-          event.urlAfterRedirects.includes(route)
-        );
-      });
+  constructor(private router: Router, private auth: AuthService) {}
+
+  getIsAuthenticated(): boolean {
+    return this.auth.isAuthenticated();
   }
+
 }
-
-//export class AppComponent {
-  //title = 'byteassist-frontend';
-  //activeSection: string = 'home';
-
-  //constructor(private router: Router) {}
-
-//}
