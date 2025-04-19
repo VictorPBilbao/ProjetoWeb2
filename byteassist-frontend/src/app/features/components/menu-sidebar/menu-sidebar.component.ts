845e60e2a127b0c10661d6724275a1afef81d35c
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-04-19 13:07:50 -0300

    bugfix(sidebar): improve sidebar functionality and styling for better responsiveness

--- byteassist-frontend/src/app/features/components/menu-sidebar/menu-sidebar.component.ts
+++ byteassist-frontend/src/app/features/components/menu-sidebar/menu-sidebar.component.ts
@@ -18,4 +18,32 @@ export class MenuSidebarComponent {
       .join('')
       .toUpperCase();
   }
+
+  openSidebar(): void {
+    const widthScreen = window.innerWidth;
+
+    if (widthScreen <= 768) {
+      const sidebar = document.querySelector('.sidebar') as HTMLElement;
+      const textsSidebar = document.querySelectorAll('.nav-link-text') as NodeListOf<HTMLElement>;
+      const menuUserName = document.querySelector('.menu-user-name') as HTMLElement;
+      const menuSearchContainer = document.querySelector('.menu-search-container') as HTMLElement;
+      var opacity = "0";
+      var width = "0px";
+
+      opacity = sidebar.classList.contains('open') ? '0' : '1';
+      width = sidebar.classList.contains('open') ? '0px' : 'auto';
+      sidebar.classList.toggle('open');
+
+      textsSidebar.forEach((text) => {
+        text.style.opacity = opacity;
+        text.style.width = width;
+      });
+
+      menuUserName.style.opacity = opacity;
+      menuUserName.style.width = width;
+
+      menuSearchContainer.style.opacity = opacity;
+      menuSearchContainer.style.width = width;
+    }
+  }
 }
