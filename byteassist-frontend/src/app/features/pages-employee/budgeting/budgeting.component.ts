4db53274d7e72ea882e57b8f4d0a22bb931cf860
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-04-22 23:05:50 -0300

    refactor: budget component styles and layout; add responsive design features
    - Updated budget.component.css to improve table header width and added media queries for better responsiveness on smaller screens.
    - Introduced a new class for table header styling to enhance visual hierarchy.
    - Modified budget.component.html to apply new styling class to the table header.
    - Implemented a new method in user.service.ts to fetch user rules based on user ID.
    - Added new images for sidebar menu options including customer support, home, payment, technical support, user, and value chain.

--- byteassist-frontend/src/app/features/pages-employee/budgeting/budgeting.component.ts
+++ byteassist-frontend/src/app/features/pages-employee/budgeting/budgeting.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
+import { Budget } from '../../shared/models/budget.model';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { BudgetService } from '../../services/budget/budget.service';
+import { NotificationComponent } from '../../components/notification/notification.component';
+import ptBr from '@angular/common/locales/pt';
+
+registerLocaleData(ptBr);
 
 @Component({
   selector: 'app-budgeting',
-  imports: [],
+  imports: [
+    NotificationComponent,
+    CommonModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   templateUrl: './budgeting.component.html',
   styleUrl: './budgeting.component.css'
 })
 export class BudgetingComponent {
+  budgets: Budget[] = [];
+  activeAccordion: number | null = null;
+  modalVisible = false;
+  selectedBudget: Budget | null = null;
+  message: string = '';
+  showNotification: boolean = false;
 
+  constructor(private budgetService: BudgetService) { }
+
+  ngOnInit(): void {
+    this.budgets = this.budgetService.getBudgets();
+  }
+
+  openBudget(index: number): void {
+    this.activeAccordion = this.activeAccordion === index ? null : index;
+  }
 }
