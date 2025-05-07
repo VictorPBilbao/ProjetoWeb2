ef328c7b5ed47906e3506f7ef62fa4cd24ae6328
Victor Pasini Bilbao <victor.pasini.bilbao@gmail.com>
2025-05-07 16:54:56 -0300

    feat: enhance validation groups by extending Default for Create and Update interfaces

--- byteassist-backend/src/main/java/com/ufpr/byteassist_backend/validation/ValidationGroups.java
+++ byteassist-backend/src/main/java/com/ufpr/byteassist_backend/validation/ValidationGroups.java
@@ -1,6 +1,8 @@
 package com.ufpr.byteassist_backend.validation;
 
+import jakarta.validation.groups.Default;
+
 public class ValidationGroups {
-    public interface Create {}
-    public interface Update {}
+    public interface Create extends Default {}
+    public interface Update extends Default {}
 }
