61d873db61dea533ea6f465ed6af4dbe64bd7ad6
Victor Bilbao <89294396+VictorPBilbao@users.noreply.github.com>
2025-06-05 11:28:36 +0000

    update cors policy for github codespaces

--- byteassist-backend/src/main/java/com/ufpr/byteassist_backend/config/SecurityConfig.java
+++ byteassist-backend/src/main/java/com/ufpr/byteassist_backend/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
